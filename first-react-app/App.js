72fe2aab99da069e2d2334624102ee0e16878829
Drilon Mahmudi <drilonmahmudi@gmail.com>
2026-04-11 10:29:47 +0200

    avv

--- first-react-app/App.js
+++ first-react-app/App.js
@@ -1,30 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import MainScreen from './screens/MainScreen';
 import TestScreen from './screens/TestScreen';
 import ExerciseScreen from './screens/ExerciseScreen';
+import ListScreen from './screens/ListScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen name="Home" component={MainScreen}></Stack.Screen> */}
-        {/* <Stack.Screen name="Test" component={TestScreen}></Stack.Screen> */}
-        <Stack.Screen name="Exercise" component={ExerciseScreen}></Stack.Screen>
+      <StatusBar style="auto" />
+      <Stack.Navigator
+        initialRouteName="List"
+        screenOptions={{
+          title: 'App',
+        }}
+      >
+        <Stack.Screen name="Main" component={MainScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
+        <Stack.Screen name="Exercise" component={ExerciseScreen} />
+        <Stack.Screen name="List" component={ListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
